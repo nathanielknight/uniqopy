33bb24c6adae21f908588dc77a96ca13970b1505
Nathaniel Knight <nathaniel.ep@gmail.com>
2022-02-04 23:30:50 +0000

    Improve usage message

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 use std::{env, path::Path, process::exit};
 
-const USAGE: &str = r#"usage: uniqopy <file>"#;
+const USAGE: &str = r#"
+usage: uniqopy <file>
+
+Create a copy of a file incorporating its MD5 hash and the current
+UTC timestamp into the new file's name. The file's extension will
+be retained.
+
+Examples:
+    example -> example.2022-02-02-22:22:22.d41d8cd98f00b204e9800998ecf8427e
+    example.txt -> example.2022-02-02-22:22:22.d41d8cd98f00b204e9800998ecf8427e.txt
+"#;
 
 /// Calculate the MD5 of a stream of input data. Used to get a (reasonably)
 /// unique signature for each input file.
@@ -44,14 +54,13 @@ fn new_name(fname: &Path, ts: &str, md5: &str) -> Result<String, &'static str> {
     };
 
     let new_name = match fpath.extension() {
-        Some(ext) => format!("{}.{}.{}.{}", fname, ts, md5, ext.to_string_lossy()),
+        Some(ext) => format!("{}.{}.{}.{}", &fname, ts, md5, ext.to_string_lossy()),
         None => format!("{}.{}.{}", fname, ts, md5),
     };
     Ok(new_name)
 }
 
 fn main() {
-    // Parse arguments
     let args: Vec<String> = env::args().collect();
     let fpath = match &args[..] {
         [_, fname] => Path::new(fname),
